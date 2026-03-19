fc693648f8e61df41c9d82fbcadab6738e41c752
bzelaznicki <bartosz@zelaznicki.com>
2026-03-19 01:08:42 +0100

    first setup

---
+++ src/main/java/com/zelaznicki/bzBuzz/BzBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.zelaznicki.bzBuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BzBuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zelaznicki/bzBuzz/BzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelaznicki.bzBuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
