36350263342e296a27c572b1f304b7fcd962e6e4
bzelaznicki <bartosz@zelaznicki.com>
2026-03-19 23:33:43 +0100

    post votes schema

---
+++ src/main/resources/db/migration/V5__create_post_votes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE post_votes (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+    vote_type INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    UNIQUE (user_id, post_id),
+    CONSTRAINT chk_vote_type CHECK (vote_type IN (1, -1))
+);
+
