ec9490f2cbc915b65fcbc28de1b1928bc49c0312
bzelaznicki <bartosz@zelaznicki.com>
2026-03-19 23:45:10 +0100

    Post vote Entity and Repository

---
+++ src/main/java/com/zelaznicki/bzBuzz/post/PostVoteRepository.java
@@ -0,0 +1,23 @@
+package com.zelaznicki.bzBuzz.post;
+
+import com.zelaznicki.bzBuzz.board.Board;
+import com.zelaznicki.bzBuzz.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PostVoteRepository extends JpaRepository<PostVote, UUID> {
+
+    Optional<PostVote> findByPostAndUser(Post post, User user);
+    boolean existsByPostAndUser(Post post, User user);
+
+    @Modifying
+    @Transactional
+    void deleteByPostAndUser(Post post, User user);
+
+}
