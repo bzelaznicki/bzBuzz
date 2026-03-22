309e00b470a6f848e0f65bda63e40a098aa48054
bzelaznicki <bartosz@zelaznicki.com>
2026-03-22 20:50:20 +0100

    comment repository

---
+++ src/main/java/com/zelaznicki/bzBuzz/comment/CommentRepository.java
@@ -0,0 +1,28 @@
+package com.zelaznicki.bzBuzz.comment;
+
+import com.zelaznicki.bzBuzz.post.Post;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    long countByPost(Post post);
+
+    List<Comment> findAllByPostAndParentIsNullOrderByVoteScoreDesc(Post post);
+    List<Comment> findAllByPostAndParentIsNullOrderByCreatedAtDesc(Post post);
+    List<Comment> findAllByParentOrderByCreatedAtAsc(Comment parent);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c from Comment c WHERE c.id = :id")
+    Optional<Comment> findByIdForUpdate(@Param("id") UUID id);
+
+
+}
