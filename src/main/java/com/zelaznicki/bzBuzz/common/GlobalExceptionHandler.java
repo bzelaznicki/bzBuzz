c3b99c49fb0855094cf50b9a6d8d314d4cae50d4
bzelaznicki <bartosz@zelaznicki.com>
2026-03-24 23:37:17 +0100

    400 and 403

--- src/main/java/com/zelaznicki/bzBuzz/common/GlobalExceptionHandler.java
+++ src/main/java/com/zelaznicki/bzBuzz/common/GlobalExceptionHandler.java
@@ -1,13 +1,28 @@
 package com.zelaznicki.bzBuzz.common;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequest(IllegalArgumentException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error/400";
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleResponseStatusException(AccessDeniedException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error/403";
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -16,7 +31,7 @@ public class GlobalExceptionHandler {
         return "error/404";
     }
 
-    @ExceptionHandler(InternalError.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleInternalServerError(IllegalArgumentException e, Model model) {
         model.addAttribute("message", e.getMessage());
