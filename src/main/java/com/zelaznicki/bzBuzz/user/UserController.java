b7f6f070b9bdc50afbaf5b386691ad413aa7837f
bzelaznicki <bartosz@zelaznicki.com>
2026-03-29 00:30:37 +0100

    added comment counts to profile page

--- src/main/java/com/zelaznicki/bzBuzz/user/UserController.java
+++ src/main/java/com/zelaznicki/bzBuzz/user/UserController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+import java.util.UUID;
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
@@ -35,9 +38,13 @@ public class UserController {
 
         Page<Post> posts = postService.findByUser(user, sort, page);
 
+        Map<UUID, Long> commentCounts = postService.getCommentCounts(posts.getContent());
+
+
         model.addAttribute("currentUser", currentUser);
         model.addAttribute("user", user);
         model.addAttribute("posts", posts);
+        model.addAttribute("commentCounts", commentCounts);
         model.addAttribute("currentPage", posts.getNumber());
         model.addAttribute("totalPages", posts.getTotalPages());
         model.addAttribute("currentSort", sort);
